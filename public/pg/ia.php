ecf35c6992725a2579ae80e0f4ddafa49fe672bc
n21b <yougarmendia@gmail.com>
2025-05-19 15:52:47 -0400

    Ajustado los links a distintas IAs

--- public/pg/ia.php
+++ public/pg/ia.php
@@ -22,24 +22,17 @@
       <h3>Creación de contenidos</h3>
       <ul class="punteo_01">
         <li><a href="https://www.canva.com/es_es/" target="_blank">Canva</a>: Diseño práctico.</li>
-        <li><a href="https://tome.app/" target="_blank">Tome</a>: Crea presentaciones</li>
         <li><a href="https://www.heygen.com/" target="_blank">HeyGen</a>: Crea audio y avatares para presentaciones.</li>
         <li><a href="https://www.slidesai.io/" target="_blank">Slides AI</a>: Creador de presentaciones.</li>
         <li><a href="https://generated.photos/faces" target="_blank">Generated faces</a>: Generador de caras aleatorias.</li>
-        <li><a href="https://www.stablediffusionai.ai/es" target="_blank">Stable Difussion</a>: Creador de imágenes.</li>
-        <li><a href="https://sketch.metademolab.com/" target="_blank">Animated drawing</a>: Dale movimiento a tus dibujos.</li>
-        <li><a href="https://www.imagine.art/" target="">Imagine</a>: Texto a imagen con un generador de arte IA.</li>
+        <li><a href="https://www.imagine.art/" target="_blank">Imagine</a>: Texto a imagen con un generador de arte IA.</li>
       </ul>
       <br>
 
       <h3>Herramientas para textos</h3>
       <ul class="punteo_01">
-        <li><a href="https://www.copy.ai/" target="_blank">Copy</a>: Automatiza flujos de trabajo.</li>
         <li><a href="https://quillbot.com/" target="_blank">Quillbot</a>: Herramienta para parafrasear.</li>
-        <li><a href="https://writesonic.com/" target="_blank">Writesonic</a>: Plataforma para la creación de contenido y SEO.</li>
-        <li><a href="https://speechify.com/es/?landing_url=https%3A%2F%2Fspeechify.com%2Fes%2F" target="_blank">Speechify</a>: Conversor de texto a voz.</li>
         <li><a href="https://ttsmaker.com/es" target="_blank">TTSMaker</a>: Texto a voz.</li>
-        <li><a href="https://murf.ai/" target="_blank">Murf</a>: Generador de voces reales.</li>
         <li><a href="https://www.deepl.com/es/translator" target="_blank">DeepL</a>: Traductor de idiomas.</li>
       </ul>
       <br>
@@ -50,7 +43,6 @@
         <li><a href="https://curipod.com/" target="_blank">Curipod</a>: Planificador de lecciones referente a cualquier tópico.</li>
         <li><a href="https://scribehow.com/" target="_blank">Scribe</a>: Creador de manuales paso a paso.</li>
         <li><a href="https://www.conker.ai/" target="_blank">Conker</a>: Creación y evaluación de pruebas especial para la sala de clases.</li>
-        <li><a href="https://beta.monic.ai/" target="_blank">Monic.ai</a>: Ayudante para estudios. Flashcards, generación de cuestionarios, quizzes...</li>
         <li><a href="https://www.questionwell.org/" target="_blank">QuestionWell</a>: Asistente de profesores para generar documentos de evaluación.</li>
       </ul>
       <br>
@@ -69,7 +61,6 @@
         <li><a href="https://play.google.com/store/apps/details?id=co.appnation.geniechat" target="_blank">GPT Genie</a>: Chat multimodal.</li>
         <li><a href="https://speechnotes.co/es/" target="_blank">Speechnotes</a>: Toma notas de lo que les vas diciendo (se necesita micrófono).</li>
         <li><a href="https://krisp.ai/" target="_blank">Krisp</a>: Asistente para reuniones y llamadas.</li>
-        <li><a href="https://chartgpt.io/" target="_blank">ChartGPT</a>: Generador de gráficos, código, conversacional...</li>
         <li><a href="https://chromewebstore.google.com/detail/scribbl-ai-meeting-notes/kmjmlilenakedodldceipdnmmnfkahni" target="_blank">Scribbl</a> (extensión Chrome): Graba, transcribe y genera resúmenes de reuniones a través de Google Meet.</li>
         <li><a href="https://chromewebstore.google.com/detail/voice-control-for-chatgpt/eollffkcakegifhacjnlnegohfdlidhn" target="_blank">Voice Control for ChatGPT</a> (extensión Chrome): Permite establecer comunicación vía comandos de voz con ChatGPT (micrófono).</li>
         <li><a href="https://support.microsoft.com/es-es/office/transcribir-las-grabaciones-7fc2efec-245e-45f0-b053-2a97531ecf57" target="_blank">Office 365 transcriptor de archivo de audio a texto</a> (Manual).</li>
